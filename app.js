1bf003fae2153525a81a6e8a79b908819a927061
blair <blairhayes@hotmail.com>
2014-09-11 20:29:14 +0000

    splatter-client

--- app.js
+++ app.js
@@ -1,56 +1,51 @@
 (function() { // We use this anonymous function to create a closure.
 
-	var app = angular.module('splatter-web', []);
+	var app = angular.module('splatter-web', ['ngResource']);
 
-
-	app.controller('UserController', function() {
-        // add your user code below
-	this.hello = "Hello World";
-	this.u = u1;
-
-
-
-	// add your user code above	
+	app.factory('User', function($resource)  {
+		return $resource('http://localhost/api/users/:id.json', {id: '@id'},
+			{'update': {method:'PUT'}} );
 	});
 
+        
+	app.controller('UserController', function(User) {
+                this.data = {};
+		this.ulist = User.query();
+		this.getUser = function(i) {
+			return User.get({id: i});
+		};
 
-        // add your form controller below
+		this.login = function() {
+			this.loggedin_user = this.getUser(this.data.id);
+			this.data = {};
+		};
+		
+		this.updateUser = function() {
+			this.loggedin_user.name = this.data.name;
+			this.loggedin_user.email = this.data.email;
+			this.loggedin_user.blurb = this.data.blurb;
+			this.loggedin_user.$update();
+			this.data = {};
+		};
+
+		this.createUser = function() {
+			u = new User();
+			u.name = this.data.cname;
+			u.email = this.data.cemail;
+			u.password = this.data.cpassword;
+			u.blurb = this.data.cblurb;
+			u.$save();
+			this.data = {};
+		};
+
+		this.delUser = function() {
+			//alert("Deleting "+ this.data.delid);
+			User.delete({id: this.data.delid});
 
 
-        // add your form controller above
+		};
+			
 
-	// mock data
-        var u1 = {
-          id: 1,
-          name: "Jane Doe",
-          email: "jane@doe.com",
-          blurb: "Sometimes I feel anonymous."
-	};	
 
-        var u2 = {
-          id: 2,
-	  name: "Bob Smith",
-	  email: "bob@smith.org"
-	};
-
-       var feed = [
-         {
-	   id: 3,
-           user_id: 2,
-           body: "This is Bob's most recent splatt.",
-	   created_at: "2014-08-17T22:00:00Z"
-	 },
-         {
-	   id: 2,
-           user_id: 2,
-           body: "This is Bob's second splatt.",
-	   created_at: "2014-08-16T13:25:00Z"
-         },
-         {
-	   id: 1,
-           user_id: 2,
-           body: "This is Bob's first splatt.",
-	   created_at: "2014-08-16T10:25:00Z"
-	 }
-       ];
+	});	
 })();
